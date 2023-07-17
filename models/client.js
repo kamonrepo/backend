1a7f2850292b73796517d5e79f8018b771adf102
Marvin Bendicio <marvin.bendicio.ipi@prulifeuk.com.ph>
2023-07-18 04:25:18 +0800

    asdasdasd

--- models/client.js
+++ models/client.js
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose';
 
 const clientSchema = mongoose.Schema({
-    name: { type: String, required: true }          ,
-    contactNumber:  {type: String, required: true } ,
+    name: { type: String, required: true },
+    contactNumber:  {type: String, required: true },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'categories'},
     plan: { type: mongoose.Schema.Types.ObjectId, ref: 'plans'},
-    planName: {type: String, required: true }       ,
-    ipaddr: {type: String, required: true }         ,
-    dueDate: {type: String, required: true }        ,
-    monthlyFee: { type: String, required: true }    ,
-    address: { type: String, required: true }       ,
-    status: { type: String, default: 'Active'}      ,
+    planName: {type: String, required: true },
+    ipaddr: {type: String, required: true },
+    dueDate: {type: String, required: true },
+    monthlyFee: { type: String, required: true },
+    address: { type: String, required: true },
+    status: { type: String, default: 'Active'},
     group: { type: mongoose.Schema.Types.ObjectId, ref: 'groups'},
+    sublocId: { type: mongoose.Schema.Types.ObjectId, ref: 'sublocations'},
+    targetlocId: { type: mongoose.Schema.Types.ObjectId, ref: 'targetlocations'},
     createdAt: {
         type: Date,
         default: new Date(),
